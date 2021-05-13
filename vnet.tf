2208a151dafbf13b992483ff34e59b275d416ea1
Kevin Ziegler <kevin@kevinziegler.com>
2021-05-13 18:48:48 +0000

    Added vnet module 2

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
-module "vnet" {
-  source  = "app.terraform.io/KEVINZIEGLER-training/vnet/azurerm"
-  version = "2.4.0"
-  resource_group_name = "KaaS-rg"
+module "network" {
+  source  = "app.terraform.io/KEVINZIEGLER-training/network/azurerm"
+  version = "3.3.0"
+   resource_group_name = "test-rg"
 }
