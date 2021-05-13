03efff6eeed250591e64e7dafde996fbf4329c50
Kevin Ziegler <kevin@kevinziegler.com>
2021-05-13 18:53:09 +0000

    Added vnet module 3

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/KEVINZIEGLER-training/network/azurerm"
   version = "3.0.1"
-   resource_group_name = "test-rg"
+   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
